60d47cfaf7814982beea1200eb8ba7380436fd57
Bowen Masco <bowen@codingfoo.com>
2016-02-26 10:44:48 -0700

    Add commands to get started with an express app

---
+++ getting_started_with_express.js
@@ -0,0 +1,20 @@
+Resources for javascript are in piazza
+
+Packages have 
+semver: major, minor, patch/bugfix
+Major: Backwords breaking changes
+minor: added features
+patch: bug fixes
+fix bugs, add features, add technology
+
+node
+
+npm
+
+git clone <repo>
+
+cd <repo>
+
+npm install
+
+npm start
