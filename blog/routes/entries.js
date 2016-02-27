057d3c68271f7ad4c7a6de30772dba16212dd5e0
Bowen Masco <bowen@codingfoo.com>
2016-02-26 22:37:34 -0700

    Add blog demo head and footer files

--- blog/routes/entries.js
+++ blog/routes/entries.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET til listing. */
+
+var foo = [
+  {slug:"how to pass class", body: "come to class. do your homework"},
+  {slug:"how to fail class", body: "play video games all day"}
+];
+
+/* GET entris listing. */
 router.get('/', function(req, res, next) {
-  res.render('entries/index', { title: 'Blog' });
+  res.render('entries/index', { title: 'Blog', foo: foo });
 });
 
 module.exports = router;
